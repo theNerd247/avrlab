4f84ded10891c1110232faa7b9a4ba0c5181f7db
Douglas Smith <mcdoogen@gmail.com>
2014-10-02 16:05:15 -0400

    some changes!

--- lab2.c
+++ lab2.c
@@ -1,7 +1,16 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-  /* code */
-	return EXIT_SUCCESS;
+	DDRD = 0xFF;
+	PORTD = 0x00;
+	
+	while(1) {
+		PORTD |= (1<<0);
+		_delay_ms(500);
+		
+		PORTD &= ~(1<<0);
+		_delay_ms(500);
+	}
 }
