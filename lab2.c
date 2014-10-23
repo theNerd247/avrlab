452821896694764d2aed2ad33729e52b96ff1561
Douglas Smith <mcdoogen@gmail.com>
2014-10-23 15:41:36 -0400

    message

--- lab2.c
+++ lab2.c
@@ -3,14 +3,14 @@
 
 int main(void)
 {
-	DDRD = 0xFF;
-	PORTD = 0x00;
+	DDRB = 0xFF;
+	PORTB = 0x00;
 	
 	while(1) {
-		PORTD |= (1<<0);
+		PORTB |= (1<<0);
 		_delay_ms(500);
 		
-		PORTD &= ~(1<<0);
+		PORTB &= ~(1<<0);
 		_delay_ms(500);
 	}
 }
